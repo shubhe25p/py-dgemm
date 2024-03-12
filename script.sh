6ab1346abfc9a57b569ffeb52a3608385ce0301e
shubhp@perlmutter <shubhp@perlmutter.com>
2024-03-12 11:30:08 -0700

    new study

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-nsize=${1:-1000}
+nsize=${1:-4096}
 ml PrgEnv-gnu
 ml python
 
 export OMP_PLACES=cores
 export OMP_PROC_BIND=true
 
-numaSetting=0-15,128-143
+numaSetting=0-15,128-143,112-127,240-255
 
-for t in 32
+for t in 16 32 64 128
 do	
-	output=$(OMP_NUM_THREADS=$t numactl -C $numaSetting python python-dgemm.py --nsize $nsize)
+	output=$(OMP_NUM_THREADS=$t python python-dgemm.py --nsize $nsize --niterations 100)
 	flops=$(echo $output | grep -oP "GLOPS AVG =\K.*")
 	echo "OMP $t: $flops GLOPS"
 done
