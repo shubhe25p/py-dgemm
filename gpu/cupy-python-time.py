e03a8fdc84256c35c45b50e5ef9385fd83275bb0
shubhp nersc <shubhp@DOE7102450.local>
2024-03-27 01:03:04 -0700

    bug fic

--- gpu/cupy-python-time.py
+++ gpu/cupy-python-time.py
@@ -180,7 +180,7 @@ def report_performance(niterations, nsize, deltat_matmul ):
         i = ind[s]
         si = "{:s} ({:d})".format( s, i )
         print("{:15s}   {:7.5f} {:7.1f}".format( si, deltat_matmul[i], gflops[i] ) )
-    print("GPU AVG NORMAL TIME= {:7.2f}".format(xp.mean(np.asarray(gflops))))
+    print("GPU AVG GFLOPs = {:7.2f}".format(xp.mean(np.asarray(gflops))))
     #xp.set_printoptions(precision=2)
     #print(gflops)
 
@@ -196,7 +196,6 @@ def get_args():
     parser.add_argument("--accelerator", required=False, default=True, action='store_true', help="option to use accelerator")
     parser.add_argument("--shownumpy", required=False, action='store_true', help="show numpy configuration")
     parser.add_argument("--testseed", type=int, required=False, default=None, help="random seed for sampling matrix elements to validate (integer).")
-    parser.add_argument("--device", type=int, required=False, default=0, help="dimension of square matrix")
 
     args = parser.parse_args()
 
@@ -223,25 +222,21 @@ def main():
     niterations = args.niterations
     nsize       = args.nsize
     testseed    = args.testseed
-    device      = args.device
     
     #choose the appropriate numpy-like interface:
     xp = numpy_initializer( args.shownumpy )
-    #with xp.cuda.Device(3):
     for i in range(4):
         xp.cuda.runtime.setDevice(i)
         print("CUDA DEVICE=",xp.cuda.get_device_id())
-        for asize in [1024, 2048, 4096, 8192, 16384]:
-            print("Size of matrix: ",asize)
-            [ A, B, C ] = create_arrays( asize, xp )
-            deltat_matmul = matmul_loop( niterations, A, B, C, xp )
+        [ A, B, C ] = create_arrays( nsize, xp )
+        deltat_matmul = matmul_loop( niterations, A, B, C, xp )
 
     # check against source of truth
     #is_correct = check_correctness( nsize, A, B, C, testseed )
     #assert( is_correct )
 
     # if correctness test has passed, report performance
-            report_performance( niterations, nsize, deltat_matmul)
+        report_performance( niterations, nsize, deltat_matmul)
     #print(benchmark(xp.matmul, (A, B, C), n_repeat=niterations, devices=(device,)))
 if __name__ == '__main__':
     main()
