6f8979e797b5fa23a6084a0c45123b112634c28e
shubhp@perlmutter <shubhp@perlmutter.com>
2024-04-01 18:19:58 -0700

    new way

--- podman/script.sh
+++ podman/script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for t in 64 128 256 512 1024 2048
+for t in 128
 do
-	for nsize in 1024 2048 4096 8192 16384
+	for nsize in 16384
 	do
 		echo -e "OMP: $t with SIZE: $nsize \n"
 		OMPSTR="OMP_NUM_THREADS=$t"
 		#echo $OMPSTR
-		podman-hpc run --env $OMPSTR n10_pydgemm:1.0 python /opt/py-dgemm/python-dgemm.py --nsize $nsize --niteration 100 | grep Best
+		podman-hpc run --env $OMPSTR --env "OMP_PLACES=cores" --env "OMP_PROC_BIND=true" n10_pydgemm:1.0 python /opt/py-dgemm/python-dgemm.py --nsize $nsize --niteration 100 | grep Best
 	done
 done
 
